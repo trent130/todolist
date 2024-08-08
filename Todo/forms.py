79716c1a599ad723d0d7d20e1035ad7ecdfbb649
trent130 <warsamegift@gmal.com>
2024-08-08 22:46:21 +0300

    template makeover

--- Todo/forms.py
+++ Todo/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import TodoItem
+from django.contrib.auth.models import User
 
 class todoForm(forms.ModelForm):
     
     class Meta:
         model = TodoItem
         fields = ("title", "description", "due_date", "completed")
+        labels = {
+            "title": "Title",
+            "description": "Description",
+            "due_date": "Due_date",
+            "completed": "Completed",
+        }
         
         widgets = {
             "title": forms.TextInput(attrs={"class":"form-control", "placeholder":"Enter your title here"}),
@@ -13,3 +20,12 @@ class todoForm(forms.ModelForm):
             "due_date": forms.DateInput(attrs={"class":"form-control", "type":"date"}),
             "completed": forms.CheckboxInput(attrs={"class":"form-check-input",}),
         }
+
+class registerForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(), max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput(), max_length=150)
+    
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "password1"]
+            
--- Todo/views.py
+++ Todo/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render, reverse, get_object_or_404, login_required
+from django.shortcuts import render, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from .forms import todoForm
+from .forms import todoForm, registerForm
 from django.contrib import messages
 from .models import TodoItem
+from django.contrib.auth.models import User
 
 # Create your views here.
-# def login(request):
-#     if request.method == "POST":
-#         form = auth
+def registerview(request):
+    if request.method == "POST":
+        form = registerForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            
+            if User.objects.filter(email=email).exists():
+                messages.error(request, "user email already in use")
+                return HttpResponseRedirect(reverse("Todo:register"))
+            else:
+                messages.success(request, "registration successful")
+                login(request, user)
+                return HttpResponseRedirect(reverse("Todo:todo_list"))
+        else:
+            messages.error(request, "invalid form")
+    else:        
+        form = registerForm()
+    context={"title":"register", "form":form}
+    return render(request, "registration/register.html", context)
+            
 @login_required
 def todo_list(request):
     if request.method == "POST":
@@ -24,12 +43,13 @@ def todo_list(request):
     context = {"title":"todo_list", "items":items, "form":form}
     return render(request,"Todo/todo_list.html", context)
 
+@login_required
 def todo_details(request, pk):
     item = get_object_or_404(TodoItem, pk=pk)
     context = {"title":"todo details", "item":item}
     return render(request, "Todo/todo_details.html", context)
 
-
+@login_required
 def edit_todo(request, pk):
     item = get_object_or_404(TodoItem, pk=pk)
     if request.method == "POST":
@@ -45,9 +65,8 @@ def edit_todo(request, pk):
     context = {"title":"edit_todo", "form":form}
     return render(request, "Todo/todo_edit.html", context)
 
+@login_required
 def delete_todo(request, pk):
     item = get_object_or_404(TodoItem, pk=pk)
     item.delete()
-    return HttpResponseRedirect(reverse("Todo:todo_list"))
-    
-    
+    return HttpResponseRedirect(reverse("Todo:todo_list")) 
